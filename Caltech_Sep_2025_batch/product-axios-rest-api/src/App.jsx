f87172e855db3255f5455e0fc7328539a03f81dd
code-qtzl <azepeda90@outlook.com>
2025-09-30 08:34:19 -0500

    update to form

--- Caltech_Sep_2025_batch/product-axios-rest-api/src/App.jsx
+++ Caltech_Sep_2025_batch/product-axios-rest-api/src/App.jsx
@@ -3,9 +3,11 @@ import './App.css'
 import axios from 'axios';
 
 function App() {
+let URL="http://localhost:3000/products";
 let [products,setProducts]=useState([]);
 
 let [product,setProduct]=useState({"pname":"",price:0.0});
+let [submitButtonValue,setSubmitButtonValue]=useState("Add Product");
 
 // load the data using ES5 style 
 // let loadProductData=()=> {
@@ -21,7 +23,7 @@ let [product,setProduct]=useState({"pname":"",price:0.0});
 // ES6 style 
 let loadProductData= async ()=> {
   try{
-  let result = await axios.get("http://localhost:3000/products");
+  let result = await axios.get(URL);
   //console.log(result.data)
   setProducts(result.data)
   }catch(error){
@@ -29,31 +31,62 @@ let loadProductData= async ()=> {
   }
 }
 
-let storeProduct=async (event)=> {
+let storeOrUpdateProduct=async (event)=> {
   event.preventDefault(); 
       console.log(product)
   // 1st parameter is url and 2nd parameter json data or js object 
+  if(submitButtonValue=="Add Product"){
+
+  
   try{
-  let result = await axios.post("http://localhost:3000/products",product)
+  let result = await axios.post(URL,product)
   console.log(result)
   }catch(error){
     console.log(error)
   }
+}else {
+    //alert("ready to update")
+    try{
+      // put method 1st parameter url with id must be appended and 2nd parameter json data. 
+      let result = await axios.put(URL+"/"+product.id,product);
+      console.log(result)
+      setSubmitButtonValue("Add Product")
+    }catch(error){
+      console.log(error)
+    }
+}
+
   setProduct({pname:"",price:0.0})
 }
 loadProductData();
+
+
+let deleteProduct=async (id)=> {
+  //alert("delete fun called.."+id)
+  try{
+  let result = await axios.delete(URL+"/"+id);
+  console.log(result)
+  }catch(error){
+    console.log(error)
+  }
+}
+let readyToUpdateProduct=(product)=> {
+  //alert(product)
+  setProduct({"id":product.id,"pname":product.pname,"price":product.price})
+  setSubmitButtonValue("Update Product");
+}
   return (
     <>
     <h2>Product CRUD Operation using axios with json-server</h2>
     <h2>Store Product</h2>
-    <form onSubmit={storeProduct}>
+    <form onSubmit={storeOrUpdateProduct}>
       <label>PName</label>
       <input type='text' name='pname' value={product.pname}
       onChange={(event)=> setProduct({...product,"pname":event.target.value})} /> <br/>
       <label>Price</label>
       <input type='number' name='price' value={product.price}
       onChange={(event)=> setProduct({...product,"price":event.target.value})} /> <br/>
-      <input type='submit' value="Store Product"/>
+      <input type='submit' value={submitButtonValue}/>
       <input type='reset' value="reset"/>
     </form>
 
@@ -63,7 +96,9 @@ loadProductData();
         <tr>
           <th>PId</th>
           <th>PName</th>
-          <th>Price</th>  
+          <th>Price</th> 
+          <th>Delete</th>
+          <th>Update</th> 
         </tr>      
     </thead>
     <tbody>
@@ -73,6 +108,12 @@ loadProductData();
             <td>{p.id}</td>
             <td>{p.pname}</td>
             <td>{p.price}</td>
+            <td>
+              <input type='button' value="delete" onClick={()=>deleteProduct(p.id)}/>
+            </td>
+            <td>
+              <input type='button' value="update" onClick={()=>readyToUpdateProduct(p)}/>
+            </td>
           </tr>
         )
       }
